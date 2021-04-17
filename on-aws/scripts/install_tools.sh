8b276f10ca7cc582df25e89174cae3b793d24af6
Ahmet Turkmen <f.ahmet.turkmen@icloud.com>
2021-04-17 03:43:38 +0300

    custom ami on aws created with packer

---
+++ on-aws/scripts/install_tools.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -eux
+
+## install nmap
+echo "==> Installing nmap and net-tools"
+apt update -y
+apt install net-tools -y
+apt update -y
+apt install nmap -y
+
+
+## install ubuntu-desktop gnome
+echo "==> Installing ubuntu-desktop gnome "
+apt update -y
+apt install ubuntu-desktop -y
+
+
+## install xrdp
+echo "==> Installing xrdp"
+apt update -y
+apt install xrdp -y
+
